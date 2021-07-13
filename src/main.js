948b7c75e288bb41e8d96974fd13b7b03d082501
Pauline Freitas <paulinefrts@gmail.com>
2021-07-13 00:07:02 -0300

    primeiro commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,36 @@
-import { example } from './data.js';
+import { example } from "./data.js";
+import pokemon from "./data/pokemon/pokemon.js";
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from "./data/pokemon/pokemon.js";
 // import data from './data/rickandmorty/rickandmorty.js';
+let todosOsPokemons = data.pokemon;
+let allCards = document.getElementById("all-cards");
 
-console.log(example, data);
+
+function criarCard(pokemon) {
+  return `
+    <div class="card">
+        <p class="pokemon-name">${pokemon.name}</p>
+        <img
+            class="pokemon-img"
+            src="${pokemon.img}"
+            alt="${pokemon.name}"
+        />
+        <p class="type">Type:${pokemon.type}</p>
+        <p class="about">
+            About:${pokemon.about}
+        </p>
+        <p class="resistant">
+            Resistant: ${pokemon.resistant}
+        </p>
+    </div>
+    `;
+}
+
+let pokemonsList = ``;
+for (let index = 0; index < todosOsPokemons.length; index++) {
+  pokemonsList += criarCard(todosOsPokemons[index]);
+}
+console.log(pokemonsList);
+
+allCards.innerHTML = pokemonsList;
